23a305d298b8199a124c29ec70e1cf09ba0d399b
aquammarine <mrguysy@gmail.com>
2026-07-23 19:32:48 +0300

    feat: add month constant

--- server/src/common/constants/time.constants.ts
+++ server/src/common/constants/time.constants.ts
@@ -3,3 +3,4 @@ export const ONE_MINUTE = 60 * ONE_SECOND;
 export const ONE_HOUR = 60 * ONE_MINUTE;
 export const ONE_DAY = 24 * ONE_HOUR;
 export const ONE_WEEK = 7 * ONE_DAY;
+export const ONE_MONTH = 30 * ONE_DAY;

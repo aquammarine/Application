a0ef307610cac93e85604c2acf98a55432a5b7e7
aquammarine <mrguysy@gmail.com>
2026-07-23 23:47:00 +0300

    fix: remove docks from prod and clear cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -28,22 +28,22 @@ async function bootstrap() {
   );
   app.useGlobalFilters(new HttpExceptionFilter());
 
-  const config = new DocumentBuilder()
-    .setTitle('Event Management API')
-    .setDescription('The API documentation for the Event Management System')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Event Management API')
+      .setDescription('The API documentation for the Event Management System')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+
+    SwaggerModule.setup('api/docs', app, document);
+  }
 
   app.enableCors({
-    origin:
-      process.env.CLIENT_URL ||
-      process.env.FRONTEND_URL ||
-      'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
     credentials: true,
-    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
   });
 
   await app.listen(process.env.PORT ?? 3000);

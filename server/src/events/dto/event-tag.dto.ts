7d8f4a6e7203aafd1a9d2484819cfc57a6dc17ea
aquammarine <mrguysy@gmail.com>
2026-08-06 05:24:11 +0300

    refactor: remove dead code

--- server/src/events/dto/event-tag.dto.ts
+++
@@ -1,7 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { TagDto } from 'src/tags/dto/tag.dto';
-
-export class EventTagDto {
-  @ApiProperty() position!: number;
-  @ApiProperty({ type: TagDto }) tag!: TagDto;
-}
